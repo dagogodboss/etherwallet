45bcd63db8760208b09fcd06378d6308f11a8179
dagogodboss <dagogodboss@gmail.com>
2018-01-30 21:02:08 +0100

    hello my friend from long

--- master.js
+++ master.js
@@ -1,7 +1,43 @@
+	var ether   = require('ethers'); 
+	
+	var provider = ether.providers.getDefaultProvider();
+	function error_in_connection(){
+		return 'Error in Connection';
+	}
+
 module.exports = {
-	 function(){
-		console.log('hello')
+	 getAddressTransactionCount : function(add){
+		tranx = provider.getTransactionCount(add);
+		return tranx;
 	},
+
+	createWallet : function(){
+		var data = ether.Wallet.createRandom('tube panic two appear jelly car involve start canvas notice maze panel')
+		return data;
+	}, 
+
+	get_info : function(pvkey){
+		const 	key = pvkey,
+				wallet = new ether.Wallet(key)
+				return wallet;
+	}, 
+
+	get_bal : function(add){
+		balance = provider.getBalance(add).then(function(balance) {
+					etherString = ether.utils.formatEther(balance);
+					return etherString
+		}, error_in_connection())
+		return balance
+	},
+
+	getTransaction : function(hash){
+		return provider.getTransaction(hash);
+	},
+
+	getReceipt	 : function(hash){
+		return provider.getTransactionReceipt(hash);
+	}
+	
 }
 	
 
--- server.js
+++ server.js
@@ -4,42 +4,93 @@ var ether   = require('ethers');
 var app = express();
 
 	app.get('/', function(req, res) {
-		var data = ether.Wallet.createRandom('extraEntropy')
-		res.send(data)
+		wallet = master.createWallet()
+		res.send(wallet);
 	});
+
 	app.get('/get-add/:key', function(req, res){
-		const 	key = req.params.key,
-				wallet = new ether.Wallet(key)
+		wallet = master.get_info(req.params.key)
 				res.json(wallet)
 	});
 
 	app.get('/ether-balance/:add', function(req, res){
 		var address = req.params.add
 		var provider = ether.providers.getDefaultProvider()
-		provider.getBalance(address).then(
-			function(balance) {
+		balnce = provider.getBalance(address);
+		
+		balnce.then(function(balance) {
 			etherString = ether.utils.formatEther(balance);
-			res.send(etherString)
-		}, error_in_connection(res))
-	})
-	
-	app.get('/ether-tranx/:add', function(req, res){
-		res.send(master.getTransaction());
-	});
+			res.json(etherString);
+		});
 
-	app.get('/send-ether/:prvKey/:amount/:add', function(req, res){
-		var key = req.params.prvKey, amount = ether.utils.parseEther(req.params.amount), address = req.params.add;
-		var wallet = new ether.Wallet(key);
-		wallet.provider = ether.providers.getDefaultProvider();
-		var trans = wallet.send(address, amount)
-		trans.catch(function(error){
+		balnce.catch(function(error){
 			obj = JSON.parse(error.responseText)
 			res.send(obj.error.message);
 		})
 	})
 
-	app.get('/send-token', function(req, res){
-		res.send('hello');
+	app.get('/send-ether/:prvKey/:amount/:add', function(req, res){
+		var key = req.params.prvKey, amount = ether.utils.parseEther(req.params.amount), address = req.params.add;
+		var wallet = new ether.Wallet(key);
+		wallet.provider = ether.providers.getDefaultProvider();
+		option = {
+			gasLimit: 21000,
+    		gasPrice: ether.utils.bigNumberify("20000000000")
+		}
+		var trans = wallet.send(address, amount, option);
+		trans.then(function(hash){
+			 res.json(hash)
+		});
+
+		trans.catch(function(error){
+			obj = JSON.parse(error.responseText)
+			res.send(obj.error.message);
+		})
+	})
+	
+	app.get('/ether-tranx/:add', function(req, res){
+		transaction = master.getAddressTransactionCount(req.params.add);
+		transaction.then(function(transactionCount){
+			res.json(transactionCount)
+		})
+	});
+
+	app.get('/getTransaction/:transHash', function(req, res){
+		transaction = master.getTransaction(req.params.transHash);
+		
+		transaction.then(function(tranx) {
+    		res.json(tranx);
+		});
+
+		transaction.catch(function(error){
+			res.send(error)
+		});
+	});
+
+	app.get('/getTransactionReceipt/:hash', function(req, res){
+		receipt = master.getReceipt(req.params.hash);
+
+		receipt.then(function(tranxReceipt){
+			res.json(tranxReceipt);
+		});
+
+		receipt.catch(function(error){
+			res.send(error)
+		});
+	});
+
+	app.get('/send-token/', function(req, res){
+		var abi = require('./abi.json'), add = '0x28abc25b4d4530720b99e051e79899642c3291e3', provider = ether.providers.getDefaultProvider();
+
+		var contract = new ether.Contract(add, abi, provider);
+		var callPromise = contract.functions.transfer("0x06Ae947Db37C62F545dedF844D3c91721c38613A", "5000");
+		callPromise.then(function(obj){
+			res.send(obj)
+		})
+
+		callPromise.catch(function(error){
+			res.send(error)
+		});
 	});
 
 	app.listen(3000, function() {
